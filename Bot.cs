c3d26e3ababf49eb1330265195c10e186955f0b6
TreyPoynter <treypoynter@gmail.com>
2023-02-17 13:09:47 -0600

    set up

---
+++ Bot.cs
@@ -0,0 +1,53 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AternosBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = String.Empty;
+
+            using (FileStream fileRead = File.OpenRead("config.json"))
+            using (StreamReader sr = new StreamReader(fileRead, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            DiscordConfiguration config = new DiscordConfiguration()
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+            CommandsNextConfiguration commConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true,
+            };
+
+            Client = new DiscordClient(config);
+            Client.Ready += Client_Ready;
+
+            Commands = Client.UseCommandsNext(commConfig);
+            await Client.ConnectAsync(); // connect the bot
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AternosBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
