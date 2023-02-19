d9d3b2aa0b102185ea2b1253aa5e6e30693d8069
TreyPoynter <treypoynter@gmail.com>
2023-02-19 10:00:41 -0600

    python time

--- Commands/ServerCommands.cs
+++ Commands/ServerCommands.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace AternosBot.Commands
 {
@@ -20,11 +21,32 @@ namespace AternosBot.Commands
             InteractivityExtension interactivity = ctx.Client.GetInteractivity();
             DateTimeOffset dto = new DateTimeOffset(DateTime.UtcNow);
             string unixTime = dto.ToUnixTimeSeconds().ToString();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            string script = @"CallAPI.py";
+            bool serverIsUp = false;
 
             await ctx.Message.RespondAsync("Server Starting...");
             //Thread.Sleep(6000);
             //await ctx.Message.RespondAsync($"Server Started on {DateTime.Now.ToString("M")} - " +
             //                               $"<t:{unixTime}:t> {ctx.Member.Mention}");
+            startInfo.FileName = @"C:\Users\doodl\AppData\Local\Programs\Python\Python311\python.exe";
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            string errors = String.Empty;
+
+            using (var process = Process.Start(startInfo))
+            {
+                errors = process.StandardError.ReadToEnd();
+                serverIsUp = Convert.ToBoolean(process.StandardOutput.ReadToEnd().ToLower());
+            }
+
+            if (serverIsUp)
+            {
+                await ctx.Message.RespondAsync($"Server Started on {DateTime.Now.ToString("M")} - " +
+                                           $"<t:{unixTime}:t> {ctx.Member.Mention}");
+            }
         }
     }
 }
